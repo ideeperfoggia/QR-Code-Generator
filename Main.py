ab74257032c8e54441814e2b65a736cd76a725f6
Marco Vallario <drake118@gmail.com>
2025-03-15 12:44:36 +0100

    remove size input

--- Main.py
+++ Main.py
@@ -21,7 +21,9 @@ def generateCode():
         factory = PyPNGImage
         format = ".png"
     #Creating a QRCode object of the size specified by the user
+    
     # qr = qrcode.QRCode(version = size.get(),box_size = 10,border = 5, image_factory=factory) 
+
     qr = qrcode.QRCode(image_factory=factory) 
     qr.add_data(text.get()) #Adding the data to be encoded to the QRCode object
     qr.make(fit = True) #Making the entire QR Code space utilized
@@ -67,28 +69,28 @@ label3.place(relx=0.05,rely=0.2, relheight=0.08)
 name = Entry(Frame3,font=('Century 12'))
 name.place(relx=0.05,rely=0.3, relwidth=1, relheight=0.2)
 
-#Getting the input of the size of the QR Code
+#Getting the file format of the QR Code
 Frame4 = Frame(win,bg="DarkTurquoise")
 Frame4.place(relx=0.1,rely=0.4,relwidth=0.7,relheight=0.2)
 
-label4 = Label(Frame4,text="Enter the size from 1 to 40 with 1 being 21x21: ",bg="DarkTurquoise",fg='azure',font=('FiraMono',13,'bold'))
-label4.place(relx=0.05,rely=0.2, relheight=0.08)
-
-size = Entry(Frame4,font=('Century 12'))
-size.place(relx=0.05,rely=0.3, relwidth=1, relheight=0.2)
-
-#Getting the file format of the QR Code
-Frame5 = Frame(win,bg="DarkTurquoise")
-Frame5.place(relx=0.1,rely=0.5,relwidth=0.7,relheight=0.2)
-
-label5 = Label(Frame5,text="Enter the format of the QR Code: ",bg="DarkTurquoise",fg='azure',font=('FiraMono',13,'bold'))
+label5 = Label(Frame4,text="Enter the format of the QR Code: ",bg="DarkTurquoise",fg='azure',font=('FiraMono',13,'bold'))
 label5.place(relx=0.05,rely=0.2, relheight=0.08)
 
 fileFormat = StringVar()
-fileFormatCombo = ttk.Combobox(Frame5, textvariable=fileFormat)
+fileFormatCombo = ttk.Combobox(Frame4, textvariable=fileFormat)
 fileFormatCombo['values'] = ('SVG', 'PNG')
 fileFormatCombo.place(relx=0.05,rely=0.3, relwidth=0.95, relheight=0.2)
 fileFormatCombo.current(0) 
+
+# #Getting the input of the size of the QR Code
+# Frame5 = Frame(win,bg="DarkTurquoise")
+# Frame5.place(relx=0.1,rely=0.5,relwidth=0.7,relheight=0.2)
+
+# label5 = Label(Frame5,text="Enter the size from 1 to 40 with 1 being 21x21: ",bg="DarkTurquoise",fg='azure',font=('FiraMono',13,'bold'))
+# label5.place(relx=0.05,rely=0.2, relheight=0.08)
+
+# size = Entry(Frame5,font=('Century 12'))
+# size.place(relx=0.05,rely=0.3, relwidth=1, relheight=0.2)
 
 #Button to generate and save the QR Code
 button = Button(win, text='Generate Code',font=('FiraMono',15,'normal'),command=generateCode)
